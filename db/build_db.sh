f28508e928f2aea6fc1321a750303a551d4c7dfe
seicho <hw.th61@gmail.com>
2024-02-05 21:11:10 +0900

    Update build_db.sh

--- db/build_db.sh
+++ db/build_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo -u postgres psql -c 'create database memo_app'
-psql -h localhost -p 5432 -d memo_app -U postgres -f postgres.sql
+psql -h localhost -p 5432 -d memo_app -U postgres -f ./db/postgres.sql
